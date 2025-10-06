fcae6f9102c7af63e00eb551f868eb161d7fcfa5
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-10-06 15:05:48 -0400

    Update files
    Added export actions to context book actions.js
    Added code to  context state.js

--- src/context/book/state.js
+++ src/context/book/state.js
@@ -3,6 +3,13 @@
 // initial state should have keys bookSearchResults and favoriteBooks
 // bookSearchResults should be an empty array
 // favoriteBooks should be the value from localStorage or an empty array if localStorage value is falsy
-const initialState = {}
+const storedFavorites = localStorage.getItem('favoriteBooks')
+const favoriteBooks = storedFavorites ? JSON.parse(storedFavorites) : [];
 
-export default initialState
+const initialState = {
+    bookSearchResults: [],
+    favoriteBooks: favoriteBooks
+ 
+}
+
+export default initialState;
