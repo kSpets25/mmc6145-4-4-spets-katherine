915da36c478b73cbffb7c85b95dc9d5ec3620f12
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2025-10-07 12:00:05 -0400

    update main and reducer files

--- src/context/book/reducer.js
+++ src/context/book/reducer.js
@@ -1,7 +1,42 @@
 // TODO: import actions and implement reducer for each action
-export default function reducer() {}
+
+import { ADD_BOOK, REMOVE_BOOK, SEARCH_BOOKS } from './actions';
 
 // This helper function stores the favoriteBook state in localStorage as a string
 function saveToLocalStorage(favBooks) {
   localStorage.setItem('favoriteBooks', JSON.stringify(favBooks))
 }
+
+const Reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK: {
+      const updatedFavorites = [...state.favoriteBooks, action.payload ];
+      saveToLocalStorage(updatedFavorites);
+      return {
+        ...state,
+        favoriteBooks: updatedFavorites
+      };
+    }
+
+    case REMOVE_BOOK: {
+      const updatedFavorites = state.favoriteBooks.filter(book => book.id !== action.payload);
+      saveToLocalStorage(updatedFavorites);
+      return {
+        ...state,
+        favoriteBooks: updatedFavorites
+      };
+    }
+
+    case SEARCH_BOOKS: {
+      return {
+        ...state,
+        searchQuery: action.payload
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 // TODO: Import BookProvider component from context and wrap App with it
-import {BookProvider} from './context/BookContext';
+import { BookProvider } from './context/book';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
